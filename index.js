a546deb04fe676f6fe36022a48437ed0f5ff0e6d
alonso1298 <alonsosagrerogranados@gmail.com>
2024-11-04 16:01:27 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Entendiendo var, let y const 
+
+var miVariable = 'variable';
+
+let variable = 'variable creada con let';
+
+const constante = 'este valor es constante';
+
+console.log(constante);
