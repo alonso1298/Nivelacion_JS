c5fc4b66a604c1c03444a6baaafefc305c0dfbbf
alonso1298 <alonsosagrerogranados@gmail.com>
2024-11-08 16:35:49 -0600

    Se clases parte 2

--- clases.js
+++ clases.js
@@ -24,20 +24,29 @@ console.log('---------------------------------------');
 
 
 class Perro {
-    propiedad = 'mi propiedad'
-    constructor(estado='normal'){ // Podemos asignar valores por defecto
+    propiedad = 'mi propiedad';
+    #hambre; // Para crear propiedades privadas es con # seguido del nombre y tienen que ser definidos 
+    static estatico = 42;
+    constructor(estado='normal', hambre = false){ // Podemos asignar valores por defecto
         this.estado = estado;
+        this.#hambre = hambre;
     }
 
     hablar() {
-        console.log(`Soy un perro ${this.estado}`);
+        console.log(`Soy un perro ${this.estado} ${this.#hambre ? 'con mucha hambre!' : 'satisfecho!'}`);
+    }
+    // Los metodos estaticos son metodos que solo se pueden acceder dentro de la clase y no es necesario generar una nueva instancia de clase 
+    static comer(){
+        console.log(this.estatico,'estoy comiendo!');
     }
 }
 
+Perro.comer();
 // Las clases que tienen un constructor se tienen que instanciar con la palabra reservada 
 const feliz = new Perro('feliz');
-console.log(feliz.__proto__.hablar);
+// console.log(feliz.__proto__.hablar);
 // feliz.hablar();
+// console.log(feliz);
 const triste = new Perro('triste');
 // triste.hablar();
 const nose = new Perro();
