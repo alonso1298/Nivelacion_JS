c06ca2a8e23c91eaeb12ab368d52f8e15b65c98b
alonso1298 <alonsosagrerogranados@gmail.com>
2024-11-14 10:35:33 -0600

    Se agrega spread operator parte2

--- spread.js
+++ spread.js
@@ -21,3 +21,34 @@ console.log('---------------------------------------');
 // Tambien podemos agregar elementos 
 const arr4 = [...arr, 2, 3]; // Estas unicamente son copias ya que si posteriormente agregamos nuevos elementos a arr no se vera afectado
 console.log(arr4);
+
+console.log('---------------------------------------');
+
+// El spread operatos tambien sirve con objetos 
+
+const obj1 = { a: 1, b: 2};
+const obj2 = { b: 5, c: 'Gatito Feliz'};
+
+//Generamos una copia del obj1
+const obj3 = { ...obj1 };
+//Si modificamos obj1 no se vera reflejado en el obj3 ya que generamos una copia
+// obj1.a = 10
+console.log(obj3, obj1);
+
+console.log('---------------------------------------');
+
+const obj4 = { ...obj1, ...obj2 };
+console.log(obj4);
+
+console.log('---------------------------------------');
+
+const obj5 = {
+    ...obj1,
+    loading: true,
+    data: {
+        prop: 'lala',
+        animal: 'gatito'
+    }
+}
+
+console.log(obj5);
